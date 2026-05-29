c962348fb4d3d8799aec080266ef129f201293a1
brian2zz <84264187+brian2zz@users.noreply.github.com>
2026-05-29 10:34:36 +0700

    chore: bump package version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nexa-framework',
-    version='0.1.0',
+    version='0.1.1',
     author='Brian',
     description='A full-stack framework for Django and Vue.js applications',
     long_description=open('README.md', encoding='utf-8').read() if os.path.exists('README.md') else '',
